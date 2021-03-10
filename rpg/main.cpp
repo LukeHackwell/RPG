39acab647b62d58aacc9d27f9cfc73809cf38c71
Luke Hackwell <lukehackwell1@gmail.com>
2021-03-10 21:51:23 +0000

    add main,cpp

---
+++ rpg/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  rpg
+//
+//  Created by luke hackwell on 10/03/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hellooooooo!\n";
+    return 0;
+}
